4b80da8f3477033a7bf018328b7b07ec10453fcc
Madi Apgar <132315455+madiapgar@users.noreply.github.com>
2024-10-10 13:31:43 -0600

    adding r_env.yml to the list of created conda envs

--- workflow/create_envs.sh
+++ workflow/create_envs.sh
@@ -14,7 +14,8 @@ env_path_list=("workflow/envs/fastqc_env.yml"
 "workflow/envs/multiqc_env.yml" 
 "workflow/envs/cutadapt_env.yml" 
 "workflow/envs/bowtie_env.yml" 
-"workflow/envs/bbmap_env.yml")
+"workflow/envs/bbmap_env.yml"
+"workflow/envs/r_env.yml")
 
 echo "installing all conda environments needed for functional CRISPR screen analysis!"
 create_my_envs ${env_path_list[*]}
