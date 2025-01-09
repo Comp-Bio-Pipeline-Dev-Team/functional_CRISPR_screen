58daed79cd3edcf53db54fbe267a7ad3ccd06f27
Madi Apgar <132315455+madiapgar@users.noreply.github.com>
2025-01-08 18:28:50 -0700

    creating a python wrapper script for workflow i think

---
+++ run_workflow.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+functional_CRISPR_screen.py \
+    -c 10 \
+    --raw_seq_dir 'raw_crispr_data' \
+    --metadata_file 'metadata.csv' \
+    --bowtie_mismatches 0 \
+    --vector_seq_used 'TTGTGGAAAGGACGAAACACCG' \
+    --vector_seq_minOverlap 10 \
+    --vector_seq_error 0.2 \
+    --crispr_sgRNA_index 'reference_data/human_crispr_sgRNA_brunello/human_crispr_sg_brunello' \
+    --dry_run True
